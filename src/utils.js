18cb0089e3e2be33fa61c02f78b09d880b7444c3
AlphanetRA <alphanetra0321@gmail.com>
2021-12-15 10:39:25 +0900

    Lesson 17 Chapter 7

--- src/utils.js
+++ src/utils.js
@@ -68,3 +68,7 @@ async function transfer (from, toAddress, amountToTransfer, transferFee, token, 
   console.log(transferReceipt)
 }
 
+async function getFee(transactionType, address, token, zkSyncProvider, ethers) {
+	const feeInWei = await zkSyncProvider.getTransactionFee(transactionType, address, token)
+	return ethers.utils.formatEther(feeInWei.totalFee.toString())
+}
