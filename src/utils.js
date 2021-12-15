9de3b582f88a61d4ff2704e6c5351f6c3261b02c
AlphanetRA <alphanetra0321@gmail.com>
2021-12-15 10:51:00 +0900

    Lesson 17 Chapter 8

--- src/utils.js
+++ src/utils.js
@@ -72,3 +72,17 @@ async function getFee(transactionType, address, token, zkSyncProvider, ethers) {
 	const feeInWei = await zkSyncProvider.getTransactionFee(transactionType, address, token)
 	return ethers.utils.formatEther(feeInWei.totalFee.toString())
 }
+
+async function withdrawToEthereum (wallet, amountToWithdraw, withdrawalFee, token, zksync, ethers) {
+  const closestPackableAmount = zksync.utils.closestPackableTransactionAmount(ethers.utils.parseEther(amountToWithdraw))
+  const closestPackableFee = zksync.utils.closestPackableTransactionFee(ethers.utils.parseEther(withdrawalFee))
+  const withdraw = await wallet.withdrawFromSyncToEthereum({
+    ethAddress: wallet.address(),
+    token: token,
+    amount: closestPackableAmount,
+    fee: closestPackableFee
+  })
+  await withdraw.awaitVerifyReceipt()
+
+  console.log('ZKP verification is complete')
+}
